35dc2bcabae8d4579a5e1e5139117d3d48a604c6
zhaoxiaojun <zhaoxiaojun@dvt.dvt.com>
2017-08-18 15:43:35 +0800

    spring boot demo1 commited...

---
+++ spring-boot-demo1/src/main/java/cn/tvfan/vote/entity/VoteRecord.java
@@ -0,0 +1,73 @@
+package cn.tvfan.vote.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="t_vote_record")
+public class VoteRecord {
+
+    @Id
+    private long id;
+
+    private String ip;
+
+    private String md5;
+
+    private int votes;
+
+    private Date votedate;
+
+    private Date createtime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public Date getVotedate() {
+        return votedate;
+    }
+
+    public void setVotedate(Date votedate) {
+        this.votedate = votedate;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+}
---
+++ spring-boot-demo1/src/main/java/cn/tvfan/vote/util/Security.java
@@ -0,0 +1,101 @@
+package cn.tvfan.vote.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.Random;
+
+public class Security {
+
+    static Logger logger = LoggerFactory.getLogger(Security.class);
+
+    public static final String ENCODING = "UTF-8";
+    private static final String DIGEST_ALGORITHM = "MD5";
+    private static final String BASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String md5(String src) throws Exception {
+
+        MessageDigest md = MessageDigest.getInstance(DIGEST_ALGORITHM);
+        byte[] bytes = src.getBytes(ENCODING);
+        md.update(bytes, 0, bytes.length);
+        byte[] digest = md.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for(byte b : digest){
+            sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+    // 随机生成32位字符串
+    public static String getRandomStr() {
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 32; i++) {
+            int number = random.nextInt(BASE.length());
+            sb.append(BASE.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static String getSha1(String str){
+        if(str == null || str.length()==0){
+            return null;
+        }
+        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+            mdTemp.update(str.getBytes("UTF-8"));
+
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char buf[] = new char[j*2];
+            int k = 0;
+            for(int i=0;i<j;i++){
+                byte byte0 = md[i];
+                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                buf[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(buf);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+    public static String sign1(String params) {
+        String signature = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(params.getBytes("UTF-8"));
+            signature = byteToHex(crypt.digest());
+        }
+        catch (NoSuchAlgorithmException e) {
+            logger.error("[security]: ", e);
+        }
+        catch (UnsupportedEncodingException e) {
+            logger.error("[security]: ", e);
+        }
+
+        return signature;
+    }
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+}
