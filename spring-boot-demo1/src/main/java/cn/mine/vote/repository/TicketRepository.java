c483a9e7dd00cff76e6e9766562a1c7c138cf474
zhaoxiaojun <zhaoxiaojun@dvt.dvt.com>
2017-09-19 11:23:53 +0800

    remove spring-boot-demo1

--- spring-boot-demo1/src/main/java/cn/mine/vote/repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.mine.vote.repository;
-
-import cn.mine.vote.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-
-}
